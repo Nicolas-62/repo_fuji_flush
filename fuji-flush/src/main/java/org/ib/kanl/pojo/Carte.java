e2a7c47c460ea89db081b997a7999efa03de8b4a
Laetitia <lschoepff@outlook.fr>
2019-11-08 15:14:46 +0100

    les cartes ne sont plus dotées d'un attribut id et code java pour création de la pioche et pour retourner la premiere carte et la piocher

--- fuji-flush/src/main/java/org/ib/kanl/pojo/Carte.java
+++ fuji-flush/src/main/java/org/ib/kanl/pojo/Carte.java
@@ -2,24 +2,15 @@ package org.ib.kanl.pojo;
 
 public class Carte {
     // Variables de classes
-    protected Integer id;
     protected Integer valeur;
 
     // Constructeur
-    public Carte(int id, int valeur){
-        this.id=id;
+    public Carte(int valeur){
         this.valeur=valeur;
     }
 
     //----------------------------------------------------------Methodes-----------------------------------------------------------------------
         // Getters et Setters
-            // id
-    public Integer getId( ) {
-        return id;
-    }
-    public void setId( Integer id ) {
-        this.id = id;
-    }
             // valeur
     public Integer getValeur( ) {
                 return valeur;
--- fuji-flush/src/main/java/org/ib/kanl/pojo/Pioche.java
+++ fuji-flush/src/main/java/org/ib/kanl/pojo/Pioche.java
@@ -1,14 +1,50 @@
 package org.ib.kanl.pojo;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Pioche< Arraylist > {
+    // variables de classe
     protected Integer id;
     protected Carte carte;
-    protected Arraylist <Carte> pioche;
+    protected ArrayList pioche;
 
+    // constructeur de la pioche
     public Pioche (){
-        pioche = new Arraylist();
+        pioche=new ArrayList();
+        this.pioche = pioche;
     }
-}
-//----------------------------------------------------------Methodes-----------------------------------------------------------------------
+    //----------------------------------------------------------Methodes-----------------------------------------------------------------------
         // Getters et Setters
             // id
+        // méthode permettant de créer une pioche avec 90 cartes de base et de mélanger le contenu de l'ArrayList
+    public void creationPioche(){
+        // ajouter les 90 cartes de base à l'arraylist pioche
+        for (int i = 1; i <= 16; i++) { this.pioche.add(new Carte (2)); }
+        for (int i = 1; i <= 12; i++) { this.pioche.add(new Carte (3)); }
+        for (int i = 1; i <= 9; i++)  { this.pioche.add(new Carte (4)); }
+        for (int i = 1; i <= 8; i++)  { this.pioche.add(new Carte (5)); }
+        for (int i = 1; i <= 6; i++)  { this.pioche.add(new Carte (6)); }
+        for (int i = 1; i <= 6; i++)  { this.pioche.add(new Carte (7)); }
+        for (int i = 1; i <= 5; i++)  { this.pioche.add(new Carte (8)); }
+        for (int i = 1; i <= 4; i++)  { this.pioche.add(new Carte (9)); }
+        for (int i = 1; i <= 4; i++)  { this.pioche.add(new Carte (10)); }
+        for (int i = 1; i <= 4; i++)  { this.pioche.add(new Carte (11)); }
+        for (int i = 1; i <= 3; i++)  { this.pioche.add(new Carte (12)); }
+        for (int i = 1; i <= 3; i++)  { this.pioche.add(new Carte (13)); }
+        for (int i = 1; i <= 3; i++)  { this.pioche.add(new Carte (14)); }
+        for (int i = 1; i <= 2; i++)  { this.pioche.add(new Carte (15)); }
+        this.pioche.add(new Carte (16));
+        this.pioche.add(new Carte (17));
+        this.pioche.add(new Carte (18));
+        this.pioche.add(new Carte (19));
+        this.pioche.add(new Carte (20));
+        // mélanger l'arrayliste initialement ordonnée
+        Collections.shuffle(this.pioche);
+        }
+        // méthode permettant de prendre une carte dans la pioche et de la retourner pour en connaitre sa valeur
+    public void returnFirstPioche(){
+        return this.pioche.remove(0);
+    }
+}
+
