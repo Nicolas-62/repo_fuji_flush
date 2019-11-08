a090caf8e5960844770a8a9aa7c9dbadb5981e08
Laetitia <lschoepff@outlook.fr>
2019-11-08 11:15:54 +0100

    ajout de la classe partie

--- fuji-flush/src/main/java/org/ib/kanl/pojo/Joueur.java
+++ fuji-flush/src/main/java/org/ib/kanl/pojo/Joueur.java
@@ -1,4 +1,26 @@
 package org.ib.kanl.pojo;
 
-public class Joueur {
+public class Joueur< Arraylist > {
+    // Variables de classe
+    protected Integer id;
+    protected String pseudo;
+    protected String email;
+    protected String password;
+    protected Main main;
+
+    // Constructeurs
+    public Joueur(String pseudo, String email, String password){
+        this.pseudo=pseudo;
+        this.email=email;
+        this.password=password;
+    }
+    public Joueur(int id, String pseudo, String email, String password){
+        this.id=id;
+        this.pseudo=pseudo;
+        this.email=email;
+        this.password=password;
+    }
+    public Joueur(){}
+
+    // Méthodes
 }
---
+++ fuji-flush/src/main/java/org/ib/kanl/pojo/Partie.java
@@ -0,0 +1,10 @@
+package org.ib.kanl.pojo;
+
+public class Partie {
+    protected String UUID;
+    protected int nbJoueur;
+    protected Joueur joueur;
+    protected Pioche pioche;
+    protected Tapis tapis;
+
+}
