6f2ba21dd1ee080cb3620095515cd9d7e8f001ee
Lourdel <nlourdel62000@gmail.com>
2019-12-11 11:41:06 +0100

    commentaires sur controllers et GameService

--- fuji-play/app/controllers/Application.java
+++ fuji-play/app/controllers/Application.java
@@ -5,30 +5,42 @@ import models.Hand;
 import models.User;
 import play.mvc.Controller;
 import play.mvc.With;
+import play.templates.GroovyInlineTags;
 import services.GameService;
 import services.HandService;
 
 @With(Secure.class)
 public class Application extends Controller {
 
+	/**
+	 * Demande authentification du joueur, récupère le joueur, sa main, le jeu associé à sa main.
+	 * @return game : jeu
+	 * @return player : joueur
+	 * @return  handPlayer : min du joueur 
+	 */
     public static void index() {
         User player = Security.connectedUser();
         Hand handPlayer = HandService.getByPlayer(player);
         Game game = handPlayer.game;
         render(game, player, handPlayer);
     }
-
+    /**
+     * Met à jour le jeu en fonction de la carte jouée
+     * @param id : handPlayer.id, id de la main d'un joueur donné
+     * @param index : index de la carte présente dans la main du joueur
+     */
     public static void playCard(Long id, Integer index) {
+    	// on récupère la main du joueur donné
         Hand hand = HandService.getById(id);
+        // on récupère le jeu associé
         Game game = hand.game;
-
+        
+        // on vérifie que la requête vient bien du joueur qui doit jouer
         User player = Security.connectedUser();
         if (player.equals(game.currentPlayer)) {
-
-            GameService.playCard(hand, hand.cards.get(index));
-
+        	
+            GameService.playCard(hand, hand.cards.get(index));         
             Hand currentHandPlayer = HandService.getByPlayer(game.currentPlayer);
-
             GameService.ruleCompareAndDiscard(game, currentHandPlayer);
 
             GameService.nextPlayer(game, hand);
--- fuji-play/app/services/GameService.java
+++ fuji-play/app/services/GameService.java
@@ -7,7 +7,11 @@ import models.Hand;
 import java.util.List;
 
 public class GameService {
-
+	/**
+	 * Met à jour le currentPlayer de la partie en cours
+	 * @param game : partie en cours
+	 * @param hand : main du joueur qui vient de jouer
+	 */
     public static void nextPlayer(Game game, Hand hand) {
         int indexOfHand = game.hands.indexOf(hand);
         indexOfHand++;
@@ -18,12 +22,23 @@ public class GameService {
         game.save();
     }
 
+    /**
+     * Déplace une carte de la main vers cardP ( cardP = card Player = carte jouée)
+     * @param hand : main du joueur
+     * @param card : carte jouée
+     */
     public static void playCard(Hand hand, Card card) {
         hand.cardP = card;
         hand.cards.remove(card);
         hand.save();
     }
 
+    /**
+     * Vérifie si le prochain joueur a une carte jouée, si oui il peut
+     * la défausser sans repiocher, les joueurs ayant la même carte également.
+     * Si c'est sa dernière carte il a gagné la partie
+     * @param game : partie en cours
+     */
     public static void ruleFullTurn(Game game) {
         Hand handNextPlayer = HandService.getByPlayer(game.currentPlayer);
         if (handNextPlayer.cardP != null) {
@@ -39,7 +54,10 @@ public class GameService {
             }
         }
     }
-
+    /**
+     * Sauvegarde la partie avec son vainqueur.
+     * @param hand : la main du joueur qui a gagné
+     */
     public static void win(Hand hand) {
         Game game = hand.game;
         game.currentPlayer = null;
@@ -47,6 +65,12 @@ public class GameService {
         game.save();
     }
 
+    /**
+     * Compare les valeurs des cartes jouées, prend en compte les associations,
+     * défausse les cartes les plus faibles, ajoute des cartes aux mains défaussées
+     * @param game : le jeu en cours
+     * @param currentHand : la main du joueur qui joue
+     */ 
     public static void ruleCompareAndDiscard(Game game, Hand currentHand) {
         int[] asso = new int[21];
 
@@ -59,6 +83,7 @@ public class GameService {
 
         // Compare les valeurs des associations
         for (Hand hand : game.hands) {
+        	
             if (hand.cardP != null && calculAsso(asso, currentHand) > calculAsso(asso, hand)) {
                 discard(game, hand);
                 draw(game.deck, hand);
@@ -66,20 +91,38 @@ public class GameService {
         }
     }
 
+    /**
+     * Calcule la somme des points des cartes identiques jouées
+     * @param asso : stocke pour chaque val de carte du jeu le
+     * nombre de fois ou elle est présente (jouée) sur le tapis
+     * @param hand : une main d'un jeu
+     * @return somme des points des cartes identiques
+     */
     private static int calculAsso(int[] asso, Hand hand) {
         return hand.cardP.value * asso[hand.cardP.value];
     }
-
+    /**
+     * Récupère la partie définie en bdd (voir initial-data.yml)
+     * @return partie jouée
+     */
     public static Game getOne() {
         return Game.find("").first();
     }
-    // ajoute une carte dans une main et la supprime du deck
+    /**
+     * Ajoute une carte dans une main à partir du deck
+     * @param deck
+     * @param hand
+     */
     public static void draw(List<Card> deck, Hand hand) {
         hand.cards.add(deck.get(0));
         deck.remove(0);
         hand.save();
     }
-
+    /**
+     * Déplace la carte jouée d'une main donnée dans la défausse
+     * @param game : partie en cours
+     * @param hand : main d'un joueur
+     */
     public static void discard(Game game, Hand hand) {
         game.discard.add(hand.cardP);
         hand.cardP = null;
